2233e2de7c8c4f3b052bf90f7106b473a2d8c1a5
Jonathan Wagner <wagnerxjonathan@gmail.com>
2025-01-24 22:20:05 +0000

    update batch settings

--- edit.sh
+++ edit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=example.out
 #SBATCH --error=error.out
-#SBATCH --mem=256GB
+#SBATCH --mem=250GB
 #SBATCH --gpus=1
+#SBATCH --cpus-per-task=2
 
 # python3 -m venv .venv
 source .venv/bin/activate
