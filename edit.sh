0435b0c82227aca58637096202da80e2f4d52e51
Jonathan Wagner <wagnerxjonathan@gmail.com>
2025-01-24 17:33:19 +0000

    add script

---
+++ edit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00
+#SBATCH --output=example.out
+#SBATCH --error=error.out
+#SBATCH --mem=80GB
+#SBATCH --gpus=1
+
+python3 -m venv .venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python3 my_edit.py
+
+conda deactivate
